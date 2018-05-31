0856bd6a59f6ccf101e8032d40ba901b5712a95a
v-messer <mailto.vmesser@gmail.com>
2018-05-31 20:17:12 +0300

    Task 1 init

---
+++ task1.js
@@ -0,0 +1,18 @@
+function palindrome(argv) {
+
+	if (argv.length>2){
+		
+		var str = argv[2];
+
+		if (10000>=str.length>=1) {
+
+		  	var re = /[\W_]/g;
+		  	var lowRegStr = str.toLowerCase().replace(re, '');
+		  	var reverseStr = lowRegStr.split('').reverse().join(''); 
+
+			process.stdout.write(reverseStr === lowRegStr ? 'YES': 'NO');
+		}
+	}
+}
+
+palindrome(process.argv);
