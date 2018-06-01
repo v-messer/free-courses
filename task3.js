6438e0a9fd6838a959c495976ec5f2d80fb559f1
v-messer <mailto.vmesser@gmail.com>
2018-06-01 15:38:31 +0300

    Task 3 init

---
+++ task3.js
@@ -0,0 +1,34 @@
+function splitter(argv) {
+
+	var vowels = 'aeiouy';
+	var glasn = 'аяуюоёеэиы';
+
+	var vowelsOut = [];
+	var consonantsOut = [];
+	var numbersOut = [];
+
+
+	for (var i = 0; i<argv[2].length; i++) {
+		var inputChr = argv[2].charAt(i);
+
+		if (inputChr.toLowerCase() != inputChr.toUpperCase()) {
+			if (vowels.indexOf(inputChr.toLowerCase())!=-1 || glasn.indexOf(inputChr.toLowerCase())!=-1) {
+				vowelsOut.push(inputChr);
+			} else {
+				consonantsOut.push(inputChr);
+			}
+		} else {
+			if (!isNaN(parseInt(inputChr, 10))) {
+				numbersOut.push(inputChr);
+			}
+		}
+
+	}
+
+	return vowelsOut.join("") + " " + consonantsOut.join("") + " " + numbersOut.join("");
+}
+
+
+if (process.argv.length>2) {
+	process.stdout.write(splitter(process.argv));
+}
